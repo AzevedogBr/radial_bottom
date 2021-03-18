e1f1fd4a6759049106b3c35e9e71ec4324836cf1
ANDRE AZEVEDO GUIMARAES <40675494+AzevedogBr@users.noreply.github.com>
2021-03-17 22:42:53 -0300

    Atualizado o package do rxdart

---
+++ lib/bloc/bloc_controller.dart
@@ -0,0 +1,22 @@
+import 'package:rxdart/rxdart.dart';
+
+class BlocController {
+  var _controller = BehaviorSubject.seeded(false);
+
+  Stream<bool> get opened => _controller.stream;
+
+  Sink<bool> get changeOpened => _controller.sink;
+
+  dispose() {
+    _controller.close();
+  }
+
+  void toggle() {
+    _controller.add(!_controller.value);
+  }
+
+
+  void close(){
+	  _controller.add(false);
+  }
+}
